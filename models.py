1039d333b23409816ca17e7856b780adb6ff7b0b
navyavk05-max <navyavk05@gmail.com>
2026-01-17 23:38:42 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    STATUS_CHOICES = (
+        ('Active','Active'),
+        ('Inactive','Inactive'),
+    )
+
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=10, decimal_places=2)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default='Active'
+
+    )
+    def __str__(self):
+        return self.name
--- views.py
+++ views.py
@@ -1,16 +1,69 @@
 from django.shortcuts import render
-import json
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-
+from rest_framework import status
+from rest_framework . response import Response
+from rest_framework . views import APIView
+from .models import Employee
+from .serializers import EmployeeSerializer
 
 # Create your views here.
-@csrf_exempt
-def test_api(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        return JsonResponse({
-            "message": "API working",
-            "received": data
-        })
-    return JsonResponse({"error": "POST only"})
+# CREATE & READ by ID
+class EmployeeDetail(APIView):
+    
+    # FETCH EMPLOYEE BYID
+    def get(self, request,pk):
+        try:
+            Employee = Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            return Response(
+                {"error": "employee not found"},
+                ststus=status.HTTP_404_NOT_FOUND
+            )
+        
+        serializer = EmployeeSerializer(Employee)
+        return Response(serializer.data)  
+
+    # UPDATE EMPLOYEE  
+    def put(self, request, pk):
+        try:
+            employee = Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            return Response(
+                {"error": "employee not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )    
+        
+        serializer = EmployeeSerializer(employee, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+   
+#    DELETE
+    def delete(self, request, pk):
+        try:
+            employee = Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            return Response(
+                {"error": "Employee not found"},
+                status=status.HTTP_404_NOT_FOUND
+            )    
+        
+        employee.status = "Inactive"
+        employee.save()
+        return Response(
+            {"message": "employee marked as Inactive"},
+            status=status.HTTP_200_OK
+        )
+    
+# ADD EMPLOYEE  
+class EmployeeCreate(APIView):
+    def post(self, request):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(status="Active")
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+          
+    
